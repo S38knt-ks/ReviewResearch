f726f535c4d3a08a8f8930bd7f542d5ab0e51a90
S38knt-ks <45904137+S38knt-ks@users.noreply.github.com>
2019-11-30 23:06:13 +0900

    fix importation

--- src/review_research/nlp/singleton/__init__.py
+++ src/review_research/nlp/singleton/__init__.py
@@ -1,3 +1,5 @@
+from .stopword_dictionary_path import StopwordDictionaryPathBuilder
+from .neologd_directory_path import NeologdDirectoryPathBuilder
 from .mecab_tagger_singleton import MecabTaggerSingleton
 from .cabocha_parser_singleton import CabochaParserSingleton 
 
