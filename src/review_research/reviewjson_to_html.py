49b8c1c5952af9249b276f70f2686a56a8e83c41
S38knt-ks <45904137+S38knt-ks@users.noreply.github.com>
2019-12-06 03:16:46 +0900

    rename json_to_html.py and fix

--- src/review_research/reviewjson_to_html.py
+++ src/review_research/reviewjson_to_html.py
@@ -1,8 +1,4 @@
-"""
-    様々なプログラムで作られたjsonファイルをhtmlファイルに変換するプログラム
-    input_dir配下のすべてのjsonファイルを変換する
-"""
-
+"""review.json ファイルを html ファイルに変換するスクリプト"""
 
 import argparse
 import json
@@ -14,32 +10,25 @@ from pprint import pprint
 import pandas
 from tqdm import tqdm
 
-from . import HtmlConvertor
+from .htmlgenerator import ReviewDataConvertor
+from .misc import get_all_jsonfiles
 
 def main(args):
   input_dir = args.input_dir
-  all_files = glob.glob('{}\\**'.format(input_dir), recursive=True)
-  json_list = [pathlib.Path(f).resolve() for f in all_files
-               if pathlib.Path(f).name == 'review.json']
+  json_list = get_all_jsonfiles(input_dir, 'review.json')
 
   normalize = args.normalize
-  mark = args.no_mark
-  hc = HtmlConvertor(normalize_mode=normalize, mark=mark)
+  convertor = ReviewDataConvertor(normalize_mode=normalize)
   out_dir = pathlib.Path(args.out_dir)
   for path in tqdm(json_list, ascii=True):
     tqdm.write('\n[file] {}'.format(path))
-    html = hc.convert(path)
-    # break
-    # pprint(html)
     out_name = path.name.replace('.json', '.html')
     if normalize:
       out_name = out_name.replace('.html', '_normalized.html')
 
-    if mark:
-      out_name = out_name.replace('.html', '_mark.html')
-
     product_dir = out_dir.parent
     out_name = product_dir / out_name
+    html = convertor.convert(path)
     with out_name.open(mode='w', encoding='utf-8') as fp:
       fp.write(html)
 
@@ -50,6 +39,4 @@ if __name__ == "__main__":
   parser.add_argument('out_dir')
   parser.add_argument('--normalize',
                       action='store_true', default=False)
-  parser.add_argument('--no-mark',
-                      action='store_false', default=True)
   main(parser.parse_args())
