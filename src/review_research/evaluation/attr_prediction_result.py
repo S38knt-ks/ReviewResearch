7680265929252a913933984bcad1380bff870d22
S38knt-ks <45904137+S38knt-ks@users.noreply.github.com>
2019-12-06 03:09:21 +0900

    fix ReviewTextInfo due to result attibution

--- src/review_research/evaluation/attr_prediction_result.py
+++ src/review_research/evaluation/attr_prediction_result.py
@@ -1,9 +1,10 @@
 import pathlib
 import json
 from collections import OrderedDict
-from typing import Tuple, Dict, Any, Union, NamedTuple, NoReturn
+from typing import Tuple, Dict, Any, Union, NamedTuple, NoReturn, Optional
 
 from review_research.review import StarsDistribution
+from ..nlp import AttrExtractionResult
 
 class ReviewTextInfo(NamedTuple):
   """商品レビュー内の1文に関する情報
@@ -17,7 +18,7 @@ class ReviewTextInfo(NamedTuple):
     title (str): レビューのタイトル
     review (str): レビュー全文
     text (str): 対象としている文
-    result (Dict[str, str]): 抽出結果
+    result Optional(Dict[str, Tuple(AttrExtractionResult, ...)]): 抽出結果
   """
   review_id: int
   last_review_id: int
@@ -27,13 +28,23 @@ class ReviewTextInfo(NamedTuple):
   title: str
   review: str
   text: str
-  result: Dict[str, str]
+  result: Optional(Dict[str, Tuple(AttrExtractionResult, ...)])
+
+  @classmethod
+  def from_dictionary(cls, dictionary: Dict[str, Any]):
+    this = cls(**dictionary)
+    result_dict = this.result
+    for attr, results in result_dict.items():
+      result_dict[attr] = AttrExtractionResult(*results)
+
+    return this._replace(result=result_dict)
 
 class AttrPredictionResult(NamedTuple):
   """属性抽出予測の結果
 
   Attributes:
     input_file (Union[str, pathlib.Path]): 入力に使ったファイル名
+    category (str): 商品カテゴリ
     product (str): 商品名
     link (str): 商品レビューページへの URL
     maker (str): 企業名
@@ -44,6 +55,7 @@ class AttrPredictionResult(NamedTuple):
     texts (Tuple[ReviewTextInfo, ...]): 抽出情報
   """
   input_file: Union[str, pathlib.Path]
+  category: str
   product: str
   link: str
   maker: str
@@ -59,6 +71,7 @@ class AttrPredictionResult(NamedTuple):
     data = json.load(json_path.open('r', encoding='utf-8'),
                      object_pairs_hook=OrderedDict)
     input_file = data['input_file']
+    category = data['category']
     product = data['product']
     link = data['link']
     maker = data['maker']
@@ -66,9 +79,9 @@ class AttrPredictionResult(NamedTuple):
     stars_distribution = StarsDistribution(**data['star_distribution'])
     total_review = data['total_review']
     total_text = data['total_text']
-    texts = tuple(ReviewTextInfo(**sentence) 
-                  for sentence in data['texts'])
-    return cls(input_file, product, link, maker, average_stars,
+    texts = tuple(ReviewTextInfo.from_dictionary(text) 
+                  for text in data['texts'])
+    return cls(input_file, category, product, link, maker, average_stars,
                stars_distribution, total_review, total_text, texts)
 
   def dump(self, json_path: Union[str, pathlib.Path]) -> NoReturn:
@@ -79,6 +92,7 @@ class AttrPredictionResult(NamedTuple):
     """
     out_data = OrderedDict()
     out_data['input_file'] = str(self.input_file)
+    out_data['category'] = self.category
     out_data['product'] = self.product
     out_data['link'] = self.link
     out_data['maker'] = self.maker
